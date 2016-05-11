51b4b9fce2e61aa17b991b8fe030e6a29d3a605f
Gennaro Avolio-Toly <gennaro.avolio-toly@smashingideas.com>
2016-05-11 13:04:03 -0700

    clean projects and new test

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,6 +1,29 @@
 class ProjectsController < ApplicationController
   def index
-    projects = Project.all
-    render json: projects, status: 200
+    load_projects
+    render_json(@projects)
+  end
+
+  def show
+    load_project
+    render_json(@project)
+  end
+
+  private
+
+  def load_projects
+    @projects ||= project_scope
+  end
+
+  def load_project
+    @project ||= project_scope.find(params[:id])
+  end
+
+  def render_json(var)
+    render json: var, status: 200
+  end
+
+  def project_scope
+    Project.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
-  resources :projects, only: [:index]
+  resources :projects, only: [:index, :show]
 end
