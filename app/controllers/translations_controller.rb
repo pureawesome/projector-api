6913a0cd6e95be9c051fb57a8684bce9cdd1a681
Gennaro Avolio-Toly <gennaro.avolio-toly@smashingideas.com>
2016-06-07 23:35:07 -0700

    Load language specific json via get call

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -3,10 +3,18 @@ class TranslationsController < ApplicationController
   # before_action :authenticate_request!
 
   def index
-    @translations = TRANSLATION_STORE
-    @translations_hash = Hash.new { |h, k| h[k] = '' }
-    @translations.keys.each { |key| @translations_hash[key] << @translations[key] }
+    load_translations
+    load_translations_hash
     render_json(@translations_hash)
+  end
+
+  def language
+    load_translations
+    load_translations_hash
+    lang = params[:lang]
+    language_hash = {}
+    @translations_hash.select { |k, v| k.include? lang }.each { |k, v| language_hash[k.gsub(/#{lang}./, '').to_sym] = v }
+    render_json(language_hash)
   end
 
   def create
@@ -16,6 +24,16 @@ class TranslationsController < ApplicationController
   end
 
   private
+
+  def load_translations
+    @translations ||= TRANSLATION_STORE
+  end
+
+  def load_translations_hash
+    @translations_hash = Hash.new { |h, k| h[k] = '' }
+    @translations.keys.each { |key| @translations_hash[key] << @translations[key] }
+  end
+
   def render_json(var)
     render json: var, status: 200
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post 'authenticate', to: 'auth#authenticate'
   post 'register', to: 'auth#register'
+  get 'language', to: 'translations#language'
   # Serve websocket cable requests in-process
   # mount ActionCable.server => '/cable'
   resources :password_resets, only: [:create, :update]
